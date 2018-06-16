1201c99b471bd1feddb778cd263c480a75ddd39a
Eryk Mariankowski <dagi12@o2.pl>
2018-06-16 17:25:38 +0200

    enum test

---
+++ src/main/java/pl/edu/amu/wmi/eryk/springbootplayground/enumtest/BaseName.java
@@ -0,0 +1,13 @@
+package pl.edu.amu.wmi.eryk.springbootplayground.enumtest;
+
+/**
+ * Stworzone przez Eryk Mariankowski dnia 15.06.18.
+ */
+public enum BaseName {
+    FLOTA1;
+
+    @Override
+    public String toString() {
+        return "FLOTA";
+    }
+}
